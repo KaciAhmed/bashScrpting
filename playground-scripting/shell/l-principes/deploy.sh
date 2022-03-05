0759c552ab35aba36a147d0614ce43d2bfc43281
ahkac <ahmed.kaci@smile.fr>
2022-03-05 22:18:59 +0100

    [DEV] mise en place des conditions pour les options

--- playground-scripting/shell/l-principes/deploy.sh
+++ playground-scripting/shell/l-principes/deploy.sh
@@ -11,11 +11,23 @@
 #
 ####################################################################################
 
-echo "
-Options :
-	 - -- create : lancer des conteneurs
-	 - -- drop :  supprimer les conteneur créer par le deploy.sh
-	 - -- info :  caractéristique des conteneurs (ip, nom, user ...)
-	 - -- start : redémarrage des conteneurs
-	 - -- ansible : déploiement arborescence ansible
-"
+if [ "$1" == "--create" ];then
+	echo "notre option est --create"
+elif [ "$1" == "--drop" ] ;then
+	echo "notre option est --drop"
+elif [ "$1" == "--info" ];then
+	echo "notre option est --info"
+elif [ "$1" == "--start" ];then 
+	echo "notre option est --start"
+elif [ "$1" == "--ansible" ];then 
+	echo "notre option est --ansible"
+else	
+	echo "
+		Options :
+			 - -- create : lancer des conteneurs
+			 - -- drop :  supprimer les conteneur créer par le deploy.sh
+			 - -- info :  caractéristique des conteneurs (ip, nom, user ...)
+			 - -- start : redémarrage des conteneurs
+			 - -- ansible : déploiement arborescence ansible
+		"
+fi
